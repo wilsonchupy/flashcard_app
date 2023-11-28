50fa8db803088429567d1c310c56eda3baa8331e
Wilson Chu <wilsonpyc88@gmail.com>
2023-11-28 13:20:37 -0800

    handle user data import

--- scripts/index.js
+++ scripts/index.js
@@ -1,153 +1,76 @@
-const dataset = {
-    name: "wilson chu",
-	levels: [],
-	lastSeen: new Date(),
-	deck: [
-        {
-            question: "The tag that contains a collection of metadata elements that describe the document or provide instructions to the browser",
-            answer: "head",
-            category: "html"
-        },
-        {
-            question: "The tag that specifies an external resource that the document is connected to",
-            answer: "link",
-            category: "html"
-        },
-        {
-            question: "The tag that provides programming code for programs to be run within the document",
-            answer: "script",
-            category: "html"
-        },
-        {
-            question: "The tag that defines the display styles used to render the document content",
-            answer: "style",
-            category: "html"
-        },
-        {
-            question: "The tag that stores the document's title or name, usually displayed in the browser title bar or on a browser tab",
-            answer: "title",
-            category: "html"
-        },
-        {
-            question: "The tag that contains the entire content of the document",
-            answer: "body",
-            category: "html"
-        },
-        {
-            question: "The tag that contains closing content that concludes an article or section",
-            answer: "footer",
-            category: "html"
-        },
-        {
-            question: "The tag that marks a list of hypertext or navigation links",
-            answer: "nav",
-            category: "html"
-        },
-        {
-            question: "The css selector that matches any of the elements elem1, elem2, etc.",
-            answer: ",",
-            category: "css"
-        },
-        {
-            question: "The css selector that matches the child element that is a child of the parent element",
-            answer: ">",
-            category: "css"
-        },
-        {
-            question: "The css selector that matches elem2 that is immediately preceded by the sibling element elem1",
-            answer: "+",
-            category: "css"
-        },
-        {
-            question: "The css selector that Matches elem2 that follows the sibling element elem1",
-            answer: "~",
-            category: "css"
-        },
-        {
-            question: "The css selector that Matches elem2 that follows the sibling element elem1",
-            answer: "~",
-            category: "css"
-        },
-        {
-            question: "The css pseudo-class that selects the first descendant of the parent that matches the specified type",
-            answer: ":first-of-type",
-            category: "css"
-        },
-        {
-            question: "The css pseudo-class that selects the nth element of the parent of the specified type",
-            answer: ":nth-of-type(n)",
-            category: "css"
-        },
-        {
-            question: "The css pseudo-class that selects An element not matching the specified selector",
-            answer: ":not(selector)",
-            category: "css"
-        },
-        {
-            question: "The css pseudo-class that selects a link that has been visited by the user",
-            answer: ":visited",
-            category: "css"
-        },
-        {
-            question: "The css pseudo-class that selects a link when the mouse pointer is hovering over the element",
-            answer: ":hover",
-            category: "css"
-        },
-        {
-            question: "The css pseudo-class that selects a link when the element is receiving the focus of the keyboard or mouse pointer",
-            answer: ":focus",
-            category: "css"
-        },
-        {
-            question: "The attribute that defers script processing until after the page has been completely parsed and loaded",
-            answer: "defer",
-            category: "js"
-        },
-        {
-            question: "The attribute that tells a browser to parse the HTML and JavaScript code together",
-            answer: "async",
-            category: "js"
-        },
-        {
-            question: "The method from the document class that reference an element using its id",
-            answer: "getElementById",
-            category: "js"
-        },
-        {
-            question: "The property of an element that returns the HTML code within itself",
-            answer: "innerHTML",
-            category: "js"
-        },
-        {
-            question: "The property of an element that returns the text within itself disregarding any HTML tags",
-            answer: "textConent",
-            category: "js"
-        },
-        {
-            question: "The method of an element to inserts HTML code defined by 'text' at 'position'",
-            answer: "insertAdjacentHTML",
-            category: "js"
-        },
-        {
-            question: "The method of a Date object that returns the year",
-            answer: "getFullYear",
-            category: "js"
-        },
-        {
-            question: "The method of a Date object that returns the day of the week, where Sunday = 0, Monday = 1, etc.",
-            answer: "getDay",
-            category: "js"
-        },
-        {
-            question: "The operator that returns the remainder after dividing one item by another",
-            answer: "%",
-            category: "js"
-        },
-        {
-            question: "The method of the Math class that rounds x down to the next lowest integer",
-            answer: "floor",
-            category: "js"
-        },
-    ]
+let dataset;
+const loadButton = document.getElementById('loadButton');
+const reader = new FileReader();
+const answerInput = document.getElementsByClassName("answerInput")[0]; 
+
+reader.addEventListener(
+    "load",
+    () => {
+        dataset = JSON.parse(reader.result);
+        const profileElem = document.getElementById("profile");
+        
+        // Set 
+        const {name, lastSeen, levels} = dataset;
+        const currentLevel = levels[levels.length-1];
+        const usernameElem = document.createElement("h1");
+        const lastSeenElem = document.createElement("h1");
+        const levelElem = document.createElement("h1");
+
+        usernameElem.innerHTML = `Welcome ${name}`;
+        lastSeenElem.innerHTML = `Last save: ${lastSeen}`;
+        levelElem.innerHTML = `Level ${currentLevel.level}`;
+
+        profileElem.appendChild(usernameElem);
+        profileElem.appendChild(lastSeenElem);
+        profileElem.appendChild(levelElem);
+
+        // update user data
+        dataset.lastSeen = new Date();
+        if (!currentLevel.updateTime) {
+            currentLevel.updateTime = dataset.lastSeen;
+        }
+        startLesson();
+    },
+    false,
+);
+
+loadButton.addEventListener('click', () => {
+    const fileInput = document.getElementById('loadFileInput');
+    
+    fileInput.click();
+    fileInput.addEventListener("change", () => {
+        const file = fileInput.files[0];
+        reader.readAsText(file);
+    });
+});
+
+
+function startLesson() {
+    const questionElem = document.getElementsByClassName("question")[0]; 
+    const categoryElem = document.getElementsByClassName("category")[0]; 
+    const spanElem = document.getElementsByTagName("span"); 
+    const answer = dataset.deck[0].answer;
+
+    questionElem.innerHTML = dataset.deck[0].question;
+    categoryElem.innerHTML = dataset.deck[0].category;
+    spanElem.textContent = answer;
 }
 
+function checkAnswer(userInput, answer) {
+    if(userInput === answer) {
+        answerInput.classList.add('correctAnswer');
+        answerInput.classList.remove('incorrectAnswer');
+    } else {
+        answerInput.classList.add('incorrectAnswer');  
+        answerInput.classList.remove('correctAnswer');
+    }
+}
+
+answerInput.addEventListener('keyup', (event) => {
+    if(event.key === 'Enter') {
+        const userAnswer = answerInput.value;
+        const spanElem = document.getElementsByTagName("span"); 
+        const answer = spanElem.textContent;
+        checkAnswer(userAnswer, answer);
+    }
+});
